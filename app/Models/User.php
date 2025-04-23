7ba96f4e129f729faf492655fea3cf37a73a4102
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-04-23 15:14:32 +0200

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Enums\User\UserStatus;
+use App\Enums\User\UserType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -30,6 +31,7 @@ class User extends Authenticatable
         'status',
         'phone',
         'avatar',
+        'type'
     ];
 
     /**
@@ -52,7 +54,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'is_active' => UserStatus::class
+            'status' => UserStatus::class,
+            'type' => UserType::class
         ];
     }
 
